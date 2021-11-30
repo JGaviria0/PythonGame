33213e3c492aa9396fc01bc08dd319d63c4b3a4f
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-11-30 11:10:04 -0500

    Fixing bugs

--- models.py
+++ models.py
@@ -1,4 +1,15 @@
 from utilities import *
+
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self,dim,pos,cl):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = cl
+        self.rect = self.image.get_rect()
+        self.rect.x=pos[0]
+        self.rect.y=pos[1]
+
+
 class Bullet(pygame.sprite.Sprite):
 
     def __init__(self, pos,velx,vely, cl=BLANCO):
@@ -21,7 +32,7 @@ class Bullet(pygame.sprite.Sprite):
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self,animations,direction,action):
+    def __init__(self,animations,direction,action,posX,posY):
         pygame.sprite.Sprite.__init__(self)
         self.animations=animations
         self.action=action
@@ -29,19 +40,32 @@ class Player(pygame.sprite.Sprite):
         self.direction=direction
         self.image = animations[direction][action][0]
         self.rect = self.image.get_rect()
-        self.rect.x=100
-        self.rect.y=ALTO - 70
+        self.rect.x=posX
+        self.rect.y=posY
         self.velx=0
         self.vely=0
         self.puntos=0
         self.salud=100
+        self.blocks=[]
         
 
     def update(self):
         self.image = self.animations[self.direction][self.action][self.actualPositionOfAnimation]
         self.actualPositionOfAnimation+=1
         self.actualPositionOfAnimation= self.actualPositionOfAnimation%len(self.animations[self.direction][self.action])
+        
+        listOfCollisionableBlocks=pygame.sprite.spritecollide(self, self.blocks, False) 
+        for b in listOfCollisionableBlocks:
 
+            if self.velx >0:
+                if self.rect.right > b.rect.left:
+                    self.rect.right = b.rect.left
+                    self.velx= 0
+
+            else:
+                if self.rect.left < b.rect.right:
+                    self.rect.left = b.rect.right
+                    self.velx=0
         self.rect.x += self.velx
         if self.rect.right > ANCHO:
             self.rect.right = ANCHO
--- prueba.py
+++ prueba.py
@@ -1,90 +1,9 @@
 import pygame
 import random
 import csv
+from models import *
 
-VERDE=[0,255,0]
-ROJO=[255,0,0]
-AZUL=[0,0,255]
-BLANCO=[255,255,255]
-NEGRO=[0,0,0]
-AMARILLO=[255, 140, 0]
-NARANJA=[255, 69, 0]
-PURPURA=[128, 0, 128]
-ROSADO=[255, 192, 203]
-ANCHO=800
-ALTO=600
 
-class Jugador(pygame.sprite.Sprite):
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([50,50])
-        self.image.fill(BLANCO)
-        self.rect = self.image.get_rect()
-        self.rect.x=200
-        self.rect.y=ALTO - 300
-        self.velx=0
-        self.vely=0
-        self.bloques=pygame.sprite.Group()
-        self.puntos=0
-        self.salud=100
-        
-
-    def update(self):
-        self.rect.x += self.velx
-        if self.rect.right > ANCHO:
-            self.rect.right = ANCHO
-            self.velx=0
-
-        if self.rect.left <= 0:
-            self.rect.left = 0
-            self.velx=0
-
-        ls_col=pygame.sprite.spritecollide(self, self.bloques, False) 
-        for b in ls_col:
-
-            if self.velx >0:
-                if self.rect.right > b.rect.left:
-                    self.rect.right = b.rect.left
-                    self.velx= 0
-
-            else:
-                if self.rect.left < b.rect.right:
-                    self.rect.left = b.rect.right
-                    self.velx=0
-        
-        self.rect.y += self.vely
-        if self.rect.bottom > ALTO:
-            self.rect.top = 0
-            self.vely=0
-
-        if self.rect.top < 0:
-            self.rect.bottom = ALTO
-            self.vely=0
-
-        ls_col=pygame.sprite.spritecollide(self, self.bloques, False) 
-        for b in ls_col:
-
-            if self.vely == 0:
-                continue
-
-            if self.vely > 0:
-                if self.rect.bottom > b.rect.top:
-                    self.rect.bottom = b.rect.top
-                    self.vely=0
-                    
-            else:
-                if self.rect.top < b.rect.bottom:
-                    self.rect.top = b.rect.bottom
-                    self.vely=0
-
-class Bloque(pygame.sprite.Sprite):
-    def __init__(self,dim,pos,cl):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = cl
-        self.rect = self.image.get_rect()
-        self.rect.x=pos[0]
-        self.rect.y=pos[1]
 
 def parserMap(initialX, initialY):
 
@@ -151,7 +70,7 @@ def parserMap(initialX, initialY):
 
 def parserColi(initialX, initialY):
 
-    bloques=pygame.sprite.Group()
+    blocks=pygame.sprite.Group()
 
     treeH = pygame.image.load('img/Tree.png')
     generator1 = pygame.image.load('img/Generator1.png')
@@ -187,21 +106,21 @@ def parserColi(initialX, initialY):
             pixelRow = initialX
         
         for eachtree in treesPosition:
-            b1 = Bloque ([50,61], eachtree, treeH)
-            bloques.add(b1)
+            b1 = Block ([50,61], eachtree, treeH)
+            blocks.add(b1)
 
         for eachtree in generator1Position:
-            b1 = Bloque ([41,55], eachtree, generator1)
-            bloques.add(b1)
+            b1 = Block ([41,55], eachtree, generator1)
+            blocks.add(b1)
         
         for eachtree in wall1Position:
-            b1 = Bloque ([30,10], eachtree, wall1)
-            bloques.add(b1)
+            b1 = Block ([30,10], eachtree, wall1)
+            blocks.add(b1)
         
         for eachtree in CastlePosition:
             pantalla.blit(castle, (eachtree[0], eachtree[1]))  
     
-    return bloques
+    return blocks
           
 if __name__=='__main__':
     pygame.init()
@@ -215,18 +134,31 @@ if __name__=='__main__':
     lim_movAba = 550
     lim_movArr = 50
 
+    # Group Of Players 
+    players=pygame.sprite.Group()
 
-    jugadores=pygame.sprite.Group()
-    bloques=pygame.sprite.Group()
+    # Group of Enemies
+    enemies=pygame.sprite.Group()
 
-    j1=Jugador()
-    jugadores.add(j1)
+    # Group of bullets 
+    bullets = pygame.sprite.Group()
 
-    # b1=Bloque ([50,50], [1400,300])
-    # bloques.add(b1)
+    # Group of blocks
+    blocks=pygame.sprite.Group()
 
-    bloques = parserColi(0,0)
-    j1.bloques= bloques
+
+    player1=Player(character['Principal_Character'],'Right','Idle',100,50)
+    players.add(player1)
+
+
+    enemy1=Enemy(character['Green_Enemy'], 'Right', 'Attack', 0, 0, 10, True,15,50,100,'Green_Enemy')
+    enemies.add(enemy1)
+
+    # b1=Block ([50,50], [1400,300])
+    # blocks.add(b1)
+
+    blocks = parserColi(0,0)
+    player1.blocks= blocks
 
     f_posx= 0
     f_velx= -5
@@ -240,64 +172,159 @@ if __name__=='__main__':
             if event.type == pygame.QUIT:
                 fin=True
             if event.type == pygame.KEYDOWN:
-                j1.velx=0
-                j1.vely=0
-                if event.key == pygame.K_d:
-                    j1.velx = 5
-                if event.key == pygame.K_a:
-                    j1.velx = -5
-                if event.key == pygame.K_w:
-                    j1.vely = -5
-                if event.key == pygame.K_s:
-                    j1.vely= 5
-            if event.type == pygame.KEYUP:
-                j1.velx=0
-                j1.vely=0          
+                player1.velx=0
+                player1.vely=0
 
-        jugadores.update()
+                # Right Direction
+                if event.key == pygame.K_d and player1.action!='Attack':
+                    if player1.direction!='Right':
+                        player1.actualPositionOfAnimation=0
+                    
+                    player1.direction='Right'
+                    player1.action='Walk'
+                    player1.velx = 5
+                
+                 # Left Direction
+                if event.key == pygame.K_a and player1.action!='Attack':
+                    if player1.direction!='Left':
+                        player1.actualPositionOfAnimation=0
+                    
+                    player1.direction='Left'
+                    player1.action='Walk'
+                    player1.velx = -5
+                
+                # Up Direction
+                if event.key == pygame.K_w and player1.action!='Attack':
+                    if player1.direction!='Up':
+                        player1.actualPositionOfAnimation=0
+                    player1.direction='Up'
+                    player1.action='Walk'
+                    player1.vely = -5
+
+                # Down Direction
+                if event.key == pygame.K_s and player1.action!='Attack':
+                    if player1.direction!='Down':
+                        player1.actualPositionOfAnimation=0
+                    player1.direction='Down'
+                    player1.action='Walk'
+                    player1.vely = 5
+                
+                # Attack
+                if event.key == pygame.K_k:
+                    player1.action='Attack'
+                    player1.actualPositionOfAnimation=0
+                    player1.velx=0
+                    player1.vely=0
+
+
+            #Static in a position
+            if event.type == pygame.KEYUP:
+                if player1.action!='Idle' and player1.action!='Attack':
+                    player1.action='Idle'
+                    player1.actualPositionOfAnimation=0
+                player1.velx=0
+                player1.vely=0
+
+
+
+
+
+        for enemy in enemies:
+
+            # Green Enemy
+            if enemy.name=='Green_Enemy':
+                if enemy.actualPositionOfAnimation ==10 or enemy.actualPositionOfAnimation==15:
+                    enemy.isAttacking=True
+
+                if enemy.isAttacking and enemy.action=='Attack':
+                    velxBullet =0
+                    velyBullet=0
+                    #Right
+                    if enemy.direction=="Right":
+                        pos=[enemy.rect.x + 50 , enemy.rect.bottom-35]
+                        velxBullet=5
+
+                    #Lef
+                    if enemy.direction=="Left":
+                        pos=[enemy.rect.x  , enemy.rect.bottom-35]
+                        velxBullet=-5
+                        
+                    # Up
+                    if enemy.direction=="Up":
+                        pos=[enemy.rect.x + 35 , enemy.rect.bottom-50]
+                        velyBullet=-5
+
+                    # Down
+                    if enemy.direction=="Down":
+                        pos=[enemy.rect.x + 35 , enemy.rect.bottom-50]
+                        velyBullet=5
+
+                    bullet=Bullet(pos,velxBullet,velyBullet) # WE CAN CONTROL THE DIRECTON WITH THE SECOND PARAMETER
+                    bullets.add(bullet)
+                    enemy.isAttacking=False
+
+            # Skeleton Enemy
+            if enemy.name == "Skeleton_Enemy":
+                pass
+        # Check movement complete for the atack
+
+        if player1.action!='Idle':
+                if player1.action=='Attack':
+                    if player1.actualPositionOfAnimation>len(player1.animations[player1.direction]['Attack'])-2:
+                        player1.action='Idle'
+                        player1.actualPositionOfAnimation=0
+   
+
+
+
+        players.update()
+        bullets.update()
+        enemies.update()
 
         pantalla.fill(NEGRO)
         parserMap(f_posx,f_posy)
 
-        jugadores.draw(pantalla)    
-        bloques.draw(pantalla) 
+        bullets.draw(pantalla)
+        enemies.draw(pantalla)
+        players.draw(pantalla)    
+        blocks.draw(pantalla) 
 
         pygame.display.flip()
         reloj.tick(40)
-        if j1.rect.right > lim_movDer:
-            j1.rect.right = lim_movDer
+        if player1.rect.right > lim_movDer:
+            player1.rect.right = lim_movDer
 
             if f_posx > lim_ventana:
                 f_posx += f_velx
 
-                for b in bloques:
+                for b in blocks:
                     b.rect.x += f_velx
 
-        if j1.rect.left <= lim_movIzq:
-            j1.rect.left = lim_movIzq
+        if player1.rect.left <= lim_movIzq:
+            player1.rect.left = lim_movIzq
 
             if f_posx <= 0:
                 f_posx -= f_velx
 
-                for b in bloques:
+                for b in blocks:
                     b.rect.x -= f_velx
 
-        if j1.rect.bottom >= lim_movAba:
-            j1.rect.bottom = lim_movAba
+        if player1.rect.bottom >= lim_movAba:
+            player1.rect.bottom = lim_movAba
 
             if f_posy >= lim_ventanaAlto:
                 f_posy += f_vely
 
-                for b in bloques:
+                for b in blocks:
                     b.rect.y += f_vely
 
-        if j1.rect.top < lim_movArr:
-            j1.rect.top = lim_movArr
+        if player1.rect.top < lim_movArr:
+            player1.rect.top = lim_movArr
 
             if f_posy <= 0:
                 f_posy -= f_vely
 
-                for b in bloques:
+                for b in blocks:
                     b.rect.y -= f_vely
 
      
