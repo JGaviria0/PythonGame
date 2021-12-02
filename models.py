b26d8c36167300705b7d32adfcf7b47d8ad32918
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-12-02 13:23:20 -0500

    Knife

--- models.py
+++ models.py
@@ -11,6 +11,21 @@ class Generator(pygame.sprite.Sprite):
         self.path = path*1
 
 
+class Knife(pygame.sprite.Sprite):
+    def __init__(self,pos,animations):
+        pygame.sprite.Sprite.__init__(self)
+        self.animations=animations
+        self.actualPositionOfAnimation=0
+        self.image = animations[0]
+        self.rect = self.image.get_rect()
+        self.rect.y=pos[1]
+        self.rect.x=pos[0]
+    def update(self):
+        self.image = self.animations[self.actualPositionOfAnimation]
+        self.actualPositionOfAnimation+=1
+        self.actualPositionOfAnimation= self.actualPositionOfAnimation%len(self.animations)
+
+
 class Magic_Book(pygame.sprite.Sprite):
     def __init__(self,pos,animations,description):
         pygame.sprite.Sprite.__init__(self)
