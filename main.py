c36257f05557b533e12b68585dce770ca79688b6
Jhon Alex Gaviria Tobon <j.gaviria@utp.edu.co>
2021-12-02 23:23:00 -0500

    check

--- main.py
+++ main.py
@@ -21,10 +21,9 @@ if __name__=='__main__':
     pygame.init()
 
     # pygame.mixer.init()
-    # sonido_fondo = pygame.mixer.Sound("sounds/End.wav")
+    # sonido_fondo = pygame.mixer.Sound("sounds/background.wav")
     # pygame.mixer.Sound.play(sonido_fondo)
-   
-
+    
     pantalla=pygame.display.set_mode([ANCHO,ALTO])
     pygame.font.init() 
                   
